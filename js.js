a8f59db71e3d8a595cb08b1d9c60d2b71c7bc964
Israr <syedfaraz173@gmail.com>
2023-11-01 19:43:45 +0530

    Scroll Trigger Work hi nahi kr raha hai :(

--- js.js
+++ js.js
@@ -1,12 +1,34 @@
-const scroll = new LocomotiveScroll({
-    el: document.querySelector('.main'),
-    smooth: true
-});
-document.addEventListener('mousemove', handleMouseMovement);
+function loco(){
+    const locoScroll = new LocomotiveScroll({
+        el: document.querySelector(".main"),
+        smooth: true
+      });
+      
+      locoScroll.on("scroll", ScrollTrigger.update);
+      
+      // tell ScrollTrigger to use these proxy methods for the ".main" element since Locomotive Scroll is hijacking things
+      ScrollTrigger.scrollerProxy(".main", {
+        scrollTop(value) {
+          return arguments.length ? locoScroll.scrollTo(value, 0, 0) : locoScroll.scroll.instance.scroll.y;
+        }, // we don't have to define a scrollLeft because we're only scrolling vertically.
+        getBoundingClientRect() {
+          return {top: 0, left: 0, width: window.innerWidth, height: window.innerHeight};
+        },
+        // LocomotiveScroll handles things completely differently on mobile devices - it doesn't even transform the container at all! So to get the correct behavior and avoid jitters, we should pin things with position: fixed on mobile. We sense it by checking to see if there's a transform applied to the container (the LocomotiveScroll-controlled element).
+        pinType: document.querySelector(".main").style.transform ? "transform" : "fixed"
+      });
+      
+      ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
+      
+      ScrollTrigger.refresh();
+      
+}
+loco();
 
+gsap.registerPlugin(ScrollTrigger);
+document.addEventListener('mousemove', handleMouseMovement);
 let circleX = 0;
 let circleY = 0;
-
 function updateCirclePosition(x, y) {
     gsap.to("#Minicircle", {
         x: x+20,
@@ -15,7 +37,6 @@ function updateCirclePosition(x, y) {
         ease: "power2.out"
     });
 }
-
 function handleMouseMovement(event) {
     const x = event.clientX;
     const y = event.clientY;
@@ -27,26 +48,16 @@ function handleMouseMovement(event) {
 
     updateCirclePosition(circleX, circleY);
 }
-gsap.registerPlugin(ScrollTrigger);
+function animateElements(selector) {
+    gsap.from(selector, {
+        y: 100,
+        opacity: 0,
+        delay: 0.5,
+        duration: 0.5,
+        stagger: 0.2,
+    });
+}
 
+animateElements(".changes");
 
-const locoScroll = new LocomotiveScroll({
-  el: document.querySelector(".main"),
-  smooth: true
-});
-
-
-gsap.from(".changes", {
-    ScrollTrigger: {
-        trigger: ".scroll-trigger", // Replace with the actual trigger element
-        start: "top",        // Set the starting position
-        end: "bottom",      // Set the ending position
-        scrub: 0.5,                // Enable scrubbing
-    },
-    y: 100,
-    opacity: 0,
-    delay: 0.5,
-    duration: 0.5,
-    stagger: 0.2,
-});
 
